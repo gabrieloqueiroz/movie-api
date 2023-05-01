9c1004512043f85a6370efa27b51950f10e28e4a
Gabriel Queiroz <gabriel-cso@hotmail.com>
2023-05-01 00:15:13 -0300

    Adding Movie Entity

--- MovieApi/Controllers/MovieController.cs
+++ MovieApi/Controllers/MovieController.cs
@@ -1,5 +1,20 @@
-﻿namespace MovieApi.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Models;
 
-public class MovieController
+namespace MovieApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController : ControllerBase
 {
+
+    private static List<Movie> movies = new List<Movie>();
+
+    [HttpPost]
+    public void insertMovie([FromBody] Movie movie)
+    {
+        movies.Add(movie);
+        Console.WriteLine(movie.Title);
+    }
+
 }
---
+++ MovieApi/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApi.Models;
+
+public class Movie
+{
+    [Required(ErrorMessage = "The field {0} is required.")]
+    public string Title { get; set; }
+    [Required(ErrorMessage = "The field {0} is required.")]
+    [MaxLength(50, ErrorMessage = "Genus length cannot be greater than 50 characters")]
+    public string Genre { get; set; }
+
+    [Required(ErrorMessage = "The field {0} is required.")]
+    [Range(70, 600, ErrorMessage = "Time must have between 70 and 600 minutes.")]
+    public int Time { get; set; }
+}
